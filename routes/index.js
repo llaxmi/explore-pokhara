b7b89712eea5633ae54f699d094107b9a281ef14
llaxmi <ll4xmi@gmail.com>
2024-07-31 15:47:36 +0545

    add required files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Pokhara" });
 });
 
+/* GET category page*/
+router.get("/category/:category", function (req, res, next) {
+  res.render("category", { title: "Pokhara", category: category });
+});
+
 module.exports = router;
