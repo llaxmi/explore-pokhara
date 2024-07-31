f70ede76182fbf6518cfd6fb7f6cc7bcc9576d7d
llaxmi <ll4xmi@gmail.com>
2024-07-31 23:41:04 +0545

    fix typo

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,24 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Destination = require('../models/destinations')
+var Destinations = require("../models/destinations");
 
 /* GET home page. */
-router.get('/add', function(req, res, next) {
-  res.render('addDestinations', { title: 'Add Destination' }); //passing value to addDestinations.ejs
+router.get("/add", function (req, res, next) {
+  res.render("addDestinations", { title: "Add Destination" });
+  //passing value to addDestinations.ejs
 });
 
-router.post('/save', function(req, res, next){
-    const destination = new Destinations(req.body);
-    destination.save();
-    res.redirect('/')
-})
-
-
-
-
-
-
-
-
+router.post("/save", function (req, res, next) {
+  const destination = new Destinations(req.body);
+  destination.save();
+  console.log(destination);
+  res.redirect("/");
+});
 
 module.exports = router;
