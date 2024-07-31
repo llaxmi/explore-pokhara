200df14a345329a6b10444c567f99d5f762b7152
Rezzuu <rezzusharma@gmail.com>
2024-07-31 20:57:32 +0545

    changes made in viewsfile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@ module.exports = {
   content: ["./views/**/*.{html,js,ejs}"],
   theme: {
     extend: {
-      // colors: {
-      //   primary: {
-      //     100: "#F5F5F5",
+      colors: {
+        primary: {
+         green: '#365E32',
       //     200: "#E5E5E5",
-      //   },
+       },
       //   secondary: {
       //     100: "#F5F5F5",
       //     200: "#E5E5E5",
-      // },
+       },
     },
   },
   plugins: [],
