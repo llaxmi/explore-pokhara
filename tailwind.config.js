780daf3d089acfd3f242aaa77531af953f8c1286
aayushmak <aayushmakafle@gmail.com>
2024-07-31 20:15:08 +0545

    second commit with add description form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,17 @@ module.exports = {
   content: ["./views/**/*.{html,js,ejs}"],
   theme: {
     extend: {
-      // colors: {
-      //   primary: {
-      //     100: "#F5F5F5",
-      //     200: "#E5E5E5",
-      //   },
-      //   secondary: {
-      //     100: "#F5F5F5",
-      //     200: "#E5E5E5",
-      // },
+      colors: {
+        primary: {
+          100: "#399918",
+          200: "#337357",
+        },
+        secondary: {
+          100: "#f2f5f1",
+          200: "#151515",
+      },
     },
   },
   plugins: [],
-};
+}
+}
