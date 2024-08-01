fb89675933f333cc78487bcd010b681db2bfedf2
Rezzuu <rezzusharma@gmail.com>
2024-08-01 17:03:59 +0545

    Made changes in the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           100: "#f2f5f1",
           200: "#151515",
         },
+
+        fontFamily: {
+          'roboto': ['Roboto', 'sans-serif'],
+        },
       },
     },
   },
