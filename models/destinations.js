1bc2ae2e9f7790c90a24de748d4ad0c9e4a296e1
Laxmi Lamichhane <68807624+llaxmi@users.noreply.github.com>
2024-08-01 14:08:40 +0545

    modify schema

--- models/destinations.js
+++ models/destinations.js
@@ -1,18 +1,23 @@
-var mongoose = require('mongoose')
-
+var mongoose = require("mongoose");
 const DestinationSchema = mongoose.Schema({
-    title: { 
-        type: String
-    },
-    location: {
-        type: String
-    },
-    description: String,
-    image: {
-        data: Buffer,  // Binary data
-        contentType: String // Mime type (e.g., 'image/png')
-    },
-    createdAt: { type: Date, default: Date.now }
+  title: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  description: String,
+  facilities: String,
+  tips: String,
+  image: {
+    data: Buffer, // Binary data
+    contentType: String, // Mime type (e.g., 'image/png')
+  },
+  category: {
+    type: String,
+    enum: ["unexplored", "cultural", "historic", "naturistic", "popular"],
+  },
+  currentStatus: String,
+  createdAt: { type: Date, default: Date.now },
 });
-
-module.exports = mongoose.model('Destinations', DestinationSchema);
+module.exports = mongoose.model("Destinations", DestinationSchema);
