c7254daef058c76a4d5bc9e11567c5e4e5d679bc
Laxmi Lamichhane <68807624+llaxmi@users.noreply.github.com>
2024-08-02 14:01:22 +0545

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Pokhara" });
-});
-
-// /* GET category page*/
-// router.get("/category/:category", function (req, res, next) {
-//   res.render("category", { title: "Pokhara", category: category });
-// });
-
-router.get("/about", function (req, res, next) {
-  res.render("about", { title: "About Pokhara" });
 });
 
 module.exports = router;
